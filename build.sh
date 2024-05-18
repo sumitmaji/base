ee1ddc5a871fd3eb475d45090d7b4151ca8146ce
Sumit Maji <skmaji1@outlook.com>
2024-05-18 14:21:46 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 source configuration
-docker build -t $IMAGE_NAME .
+source $MOUNT_PATH/kubernetes/install_k8s/util
+docker build --build-arg REGISTRY=$(fullRegistryUrl)  -t $IMAGE_NAME .
+docker tag $IMAGE_NAME $(fullRegistryUrl)/$REPO_NAME
+docker push $(fullRegistryUrl)/$REPO_NAME
