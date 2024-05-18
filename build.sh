6d7b026e49cf78ea58f6b827c2dc0f27dfb1bc0f
Sumit Maji <skmaji1@outlook.com>
2024-05-18 14:26:54 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source configuration
-docker build -t $IMAGE_NAME .
+source $MOUNT_PATH/kubernetes/install_k8s/util
+docker build --build-arg REGISTRY=$(fullRegistryUrl)  -t $IMAGE_NAME .
+docker tag $IMAGE_NAME $(fullRegistryUrl)/$REPO_NAME
+docker push $(fullRegistryUrl)/$REPO_NAME
 if [ $? -eq 0 ]
 then
   exit 0
